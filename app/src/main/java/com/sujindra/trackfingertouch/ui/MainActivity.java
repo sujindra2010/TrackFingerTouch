87ab48508d1efa973c6836e9e18bd6e604d52346
Sujindra Maharjan <sujindra2014@gmail.com>
2017-07-28 17:54:34 +0545

    'MultiTouchView' added in main activity

--- app/src/main/java/com/sujindra/trackfingertouch/ui/MainActivity.java
+++ app/src/main/java/com/sujindra/trackfingertouch/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.sujindra.trackfingertouch;
+package com.sujindra.trackfingertouch.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.sujindra.trackfingertouch.R;
 
 public class MainActivity extends AppCompatActivity {
 
